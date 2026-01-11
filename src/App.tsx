1f2df01f9526f77eedb6c73b1e355063eb2d2bfd
stepan.rutz <stepan.rutz@gmx.de>
2026-01-11 21:01:34 +0100

    use effect event

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import { SlowComponent } from './SlowComponent'
+import { useEffect, useEffectEvent, useState } from "react";
+
+
+function Counter({ count, onCountChange }: { count: number, onCountChange: (newCount: number) => void }) {
+  const worker = useEffectEvent(() => {
+    onCountChange(count + 1);
+  });
+  useEffect(() => {
+    console.log("init interval just once");
+    const i = setInterval(() => {
+      worker();
+    }, 1000);
+    return () => clearInterval(i);
+  }, []);
+  return (
+    <div className="h-1 grow self-stretch flex flex-col items-center gap-2">
+      <div>
+        Count is {count}
+      </div>
+    </div>
+  )
+}
 
 export function App() {
+  const [count, setCount] = useState(0);
   return (
-    <div className="h-1 grow self-stretch flex flex-col items-center justify-center gap-2">
-      <div>
-        Modern React / Priority Rendering
-      </div>
-      <SlowComponent title="Hello React19x" />
+    <div className="h-1 grow self-stretch flex flex-col items-center gap-2">
+      <Counter count={count} onCountChange={setCount} />
     </div>
   )
 }
