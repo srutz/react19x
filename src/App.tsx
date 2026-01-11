c7f5bf13cdcd9a5aec89b0fd3c132aab6c3e1f5d
stepan.rutz <stepan.rutz@gmx.de>
2026-01-11 10:50:50 +0100

    empty react19x with slow component

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { SlowComponent } from './SlowComponent'
+
+export function App() {
+  return (
+    <div className="h-1 grow self-stretch flex items-center justify-center p-4">
+      <SlowComponent />
+    </div>
+  )
+}
---
+++ src/SlowComponent.tsx
@@ -0,0 +1,72 @@
+import { type ReactNode, useState } from "react";
+
+export function SlowComponent() {
+  const [flip, setFlip] = useState(0);
+
+  const handleFlip = () => {
+    setFlip((prev) => prev + 1);
+  };
+
+  const TILE_COUNT = 40;
+  const TILE_SIZE = 640 / TILE_COUNT;
+  const DURATION = 160;
+  const DELAY_PER_TILE = (2 * DURATION) / (TILE_COUNT * TILE_COUNT);
+
+  const getTransform = (flip: number, y: number) => {
+    const transforms = [
+      "rotateY(0deg) rotateZ(0deg)",
+      "rotateZ(-6deg)",
+      "rotateZ(6deg)",
+      "rotateY(0deg) rotateZ(0deg)",
+      "rotateY(0deg) rotateZ(-180deg)",
+      `translateX(${200 * Math.sin(y * 2 * Math.PI / TILE_COUNT)}px) rotateY(180deg) rotateZ(180deg) scale(0.9)`,
+    ];
+    return transforms[flip % transforms.length];
+  };
+
+  const tiles: ReactNode[] = [];
+  for (let y = 0; y < TILE_COUNT; y++) {
+    for (let x = 0; x < TILE_COUNT; x++) {
+      tiles.push(
+        <img
+          src="/paris.jpg"
+          className="transition-transform ease-in-out"
+          alt="Paris"
+          key={`${x}-${y}`}
+          style={{
+            width: TILE_SIZE,
+            height: TILE_SIZE,
+            objectFit: "none",
+            objectPosition: `-${x * TILE_SIZE}px -${y * TILE_SIZE}px`,
+            transform: getTransform(flip, y),
+            transitionDelay: `${(x + y * TILE_COUNT) * DELAY_PER_TILE}ms`,
+            transitionDuration: `${DURATION}ms`,
+          }}
+        />,
+      );
+    }
+  }
+
+  return (
+    <div className="absolute overflow-hidden">
+      <button
+        type="button"
+        className="grid w-[640px] h-[640px] gap-[1px] relative"
+        onClick={handleFlip}
+        style={{
+          gridTemplateColumns: `repeat(${TILE_COUNT}, ${TILE_SIZE}px)`,
+          gridTemplateRows: `repeat(${TILE_COUNT}, ${TILE_SIZE}px)`,
+        }}
+      >
+        {tiles}
+      </button>
+      <button
+        type="button"
+        className="absolute bottom-2 right-2 py-1 px-2 bg-black bg-opacity-75 rounded"
+        onClick={handleFlip}
+      >
+        Click image to flip
+      </button>
+    </div>
+  );
+}
