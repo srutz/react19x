11db74f234fb7c6b4e76b5d1e868443e838242de
stepan.rutz <stepan.rutz@gmx.de>
2026-01-11 21:05:12 +0100

    useTransition

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,37 @@
-import { useDeferredValue, useState } from 'react'
-import { SlowComponent } from './SlowComponent'
+import { useState, useTransition } from "react";
+import { SlowComponent } from "./SlowComponent";
 
 export function App() {
-  const [title, setTitle] = useState('React 19x')
-  const [gap, setGap] = useState(1)
-  const deferredTitle = useDeferredValue(title)
-  const deferredGap = useDeferredValue(gap)
-  console.log("render App", { title, deferredTitle })
+  const [isPending, startTransition] = useTransition();
+  const [title, setTitle] = useState("React 19x");
+  const [gap, setGap] = useState(1);
+  const [deferredTitle, setDeferredTitle] = useState(title);
+  const [deferredGap, setDeferredGap] = useState(gap);
+  console.log("render App", { title, isPending });
   return (
     <div className="h-1 grow self-stretch flex flex-col items-center justify-center gap-2">
-      <div>
-        Modern React / Priority Rendering
-      </div>
+      <div>Modern React / Priority Rendering {isPending && "⏳"}</div>
 
-      <input placeholder='Type text' value={title} onChange={(e) => setTitle(e.target.value)} />
-      <input type="range" min={0} max={10} value={gap} onChange={(e) => setGap(Number.parseInt(e.target.value, 10))} />
+      <input
+        placeholder="Type text"
+        value={title}
+        onChange={(e) => {
+          setTitle(e.target.value);
+          startTransition(() => setDeferredTitle(e.target.value))
+        }}
+      />
+      <input
+        type="range"
+        min={0}
+        max={10}
+        value={gap}
+        onChange={(e) => {
+          setGap(Number.parseInt(e.target.value, 10));
+          startTransition(() => setDeferredGap(Number.parseInt(e.target.value, 10)))
+        }}
+      />
 
       <SlowComponent title={deferredTitle} gap={deferredGap} />
     </div>
-  )
+  );
 }
