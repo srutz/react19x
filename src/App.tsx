23c2dba7578cdc1bf2f47657117e4829700bd934
stepan.rutz <stepan.rutz@gmx.de>
2026-01-17 09:34:38 +0100

    activity example

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,41 @@
+import { useEffect, useState } from 'react';
 import { SlowComponent } from './SlowComponent'
 
 export function App() {
+  const [showContent, setShowContent] = useState(false);
   return (
     <div className="h-1 grow self-stretch flex flex-col items-center justify-center gap-2">
       <div>
-        Modern React / Priority Rendering
+        Modern React / Activity
       </div>
-      <SlowComponent title="Hello React19x" />
+      <button type="button" onClick={() => setShowContent((prev) => !prev)}>
+        Toggle Content
+      </button>
+      {/*
+      <Activity mode={showContent ? 'visible' : 'hidden'} >
+        <MyContent></MyContent>
+      </Activity>
+      */}
+      <div style={{ display: showContent ? 'block' : 'none' }}>
+        <MyContent></MyContent>
+      </div>
+      <div className="grow" />
+    </div>
+  )
+}
+
+
+function MyContent() {
+  console.log('MyContent render');
+  useEffect(() => {
+    console.log('MyContent mounted');
+    return () => {
+      console.log('MyContent unmounted');
+    }
+  }, []);
+  return (
+    <div className="flex flex-col items-center gap-4">
+      <SlowComponent title="Bonjour" gap={2} />
     </div>
   )
 }
